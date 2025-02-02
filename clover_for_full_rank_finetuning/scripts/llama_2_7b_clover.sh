c8da4a0d1ce1658e6541a4343478b2e1c7a411f0
fxmeng <filtergrafting@gmail.com>
2025-02-02 11:32:14 +0800

    update

--- clover_for_full_rank_finetuning/scripts/llama_2_7b_clover.sh
+++ clover_for_full_rank_finetuning/scripts/llama_2_7b_clover.sh
@@ -4,7 +4,7 @@ DATA_PATH="pissa-dataset"
 export HF_ENDPOINT=https://hf-mirror.com
 
 # batch size = per_device_train_batch_size * gradient_accumulation_steps * num_gpus = 128
-deepspeed --master_port=16971 --include=localhost:0,1,2,3,4,5,6,7 train.py \
+deepspeed --master_port=16973 --include=localhost:0,1,2,3,4,5,6,7 train.py \
     --deepspeed configs/ds_config_zero2_no_offload.json \
     --model_name_or_path $BASE_MODEL \
     --full_finetune True \
@@ -14,7 +14,7 @@ deepspeed --master_port=16971 --include=localhost:0,1,2,3,4,5,6,7 train.py \
     --sub_task commonsense \
     --dataset_split train \
     --dataset_field instruction output \
-    --output_dir $OUTPUT_PATH/ \
+    --output_dir $OUTPUT_PATH \
     --num_train_epochs 3 \
     --model_max_length 1024 \
     --per_device_train_batch_size 2 \
@@ -31,5 +31,5 @@ deepspeed --master_port=16971 --include=localhost:0,1,2,3,4,5,6,7 train.py \
     --merge True \
     --shuffle_dataset True
 
-python utils/gen_vllm.py --model $OUTPUT_PATH/ --sub_task commonsense --output_file $OUTPUT_PATH//all_response.jsonl
-python utils/test_acc.py --input_file $OUTPUT_PATH//all_response.jsonl 
+python utils/gen_vllm.py --model $OUTPUT_PATH --sub_task commonsense --output_file $OUTPUT_PATH/all_response.jsonl
+python utils/test_acc.py --input_file $OUTPUT_PATH/all_response.jsonl 
